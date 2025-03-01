77502acb0fb0e311f8908c4c892845105bfb20a1
mrd-T <2359384746@qq.com>
2025-03-01 16:38:33 +0800

    check3

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,43 +1,119 @@
 #include "tcp_sender.hh"
+#include "byte_stream.hh"
 #include "debug.hh"
 #include "tcp_config.hh"
-
+#include "tcp_sender_message.hh"
+#include <memory>
 using namespace std;
 
 // This function is for testing only; don't add extra state to support it.
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-  debug( "unimplemented sequence_numbers_in_flight() called" );
-  return {};
+  // debug( "unimplemented sequence_numbers_in_flight() called" );
+  return _bytes_in_flight;
 }
 
 // This function is for testing only; don't add extra state to support it.
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-  debug( "unimplemented consecutive_retransmissions() called" );
-  return {};
+  // debug( "unimplemented consecutive_retransmissions() called" );
+  return total_retransmission_;
 }
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
-  debug( "unimplemented push() called" );
-  (void)transmit;
+  // debug( "unimplemented push() called" );
+  while ( ( _windos_size == 0 ? 1 : _windos_size ) > _bytes_in_flight ) {
+    if ( FIN_sent_ ) {
+      break; // Is finished.
+    }
+
+    auto msg { make_empty_message() };
+    if ( not SYN_sent_ ) {
+      msg.SYN = true;
+      SYN_sent_ = true;
+    }
+
+    const uint64_t remaining { ( _windos_size == 0 ? 1 : _windos_size ) - _bytes_in_flight };
+    const size_t len { min( TCPConfig::MAX_PAYLOAD_SIZE, remaining - msg.sequence_length() ) };
+    auto&& payload { msg.payload };
+    while ( reader().bytes_buffered() != 0U and payload.size() < len ) {
+      string_view view { reader().peek() };
+      view = view.substr( 0, len - payload.size() );
+      payload += view;
+      input_.reader().pop( view.size() );
+    }
+
+    if ( not FIN_sent_ and remaining > msg.sequence_length() and reader().is_finished() ) {
+      msg.FIN = true;
+      FIN_sent_ = true;
+    }
+
+    if ( msg.sequence_length() == 0 ) {
+      break;
+    }
+
+    transmit( msg );
+    if ( !timer_.is_active() ) {
+      timer_.start();
+    }
+    next_abs_seqno_ += msg.sequence_length();
+    _bytes_in_flight += msg.sequence_length();
+    outstanding_message_.emplace( move( msg ) );
+  }
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
 {
-  debug( "unimplemented make_empty_message() called" );
-  return {};
+  return { Wrap32::wrap( next_abs_seqno_, isn_ ), false, {}, false, input_.has_error() };
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  debug( "unimplemented receive() called" );
-  (void)msg;
-}
+  if ( input_.has_error() ) {
+    return;
+  }
+  if ( msg.RST ) {
+    input_.set_error();
+    return;
+  }
 
+  _windos_size= msg.window_size;
+  if ( not msg.ackno.has_value() ) {
+    return;
+  }
+  const uint64_t recv_ack_abs_seqno { msg.ackno->unwrap( isn_, next_abs_seqno_ ) };
+  if ( recv_ack_abs_seqno > next_abs_seqno_ ) {
+    return;
+  }
+  bool has_acknowledgment { false };
+  while ( not outstanding_message_.empty() ) {
+    const auto& message { outstanding_message_.front() };
+    if ( ack_abs_seqno_ + message.sequence_length() > recv_ack_abs_seqno ) {
+      break; // Must be fully acknowledged by the TCP receiver.
+    }
+    has_acknowledgment = true;
+    ack_abs_seqno_ += message.sequence_length();
+    _bytes_in_flight -= message.sequence_length();
+    outstanding_message_.pop();
+  }
+  if ( has_acknowledgment ) {
+    total_retransmission_ = 0;
+    timer_.reload( initial_RTO_ms_ );
+    outstanding_message_.empty() ? timer_.stop() : timer_.start();
+  }
+}
 void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
 {
-  debug( "unimplemented tick({}, ...) called", ms_since_last_tick );
-  (void)transmit;
+  if ( timer_.tick( ms_since_last_tick ).is_expired() ) {
+    if ( outstanding_message_.empty() ) {
+      return;
+    }
+    transmit( outstanding_message_.front() );
+    if ( _windos_size != 0 ) {
+      total_retransmission_ += 1;
+      timer_.exponential_backoff();
+    }
+    timer_.reset();
+  }
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,16 +4,43 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
+#include <cstdint>
 #include <functional>
+#include <queue>
+
+class RetransmissionTimer
+{
+public:
+  explicit RetransmissionTimer( uint64_t initial_RTO_ms ) : RTO_ms_( initial_RTO_ms ) {}
+
+  [[nodiscard]] constexpr auto is_active() const noexcept -> bool { return is_active_; }
+  [[nodiscard]] constexpr auto is_expired() const noexcept -> bool { return is_active_ and timer_ >= RTO_ms_; }
+  constexpr auto reset() noexcept -> void { timer_ = 0; }
+  constexpr auto exponential_backoff() noexcept -> void { RTO_ms_ *= 2; }
+  constexpr auto reload( uint64_t initial_RTO_ms ) noexcept -> void { RTO_ms_ = initial_RTO_ms, reset(); };
+  constexpr auto start() noexcept -> void { is_active_ = true, reset(); }
+  constexpr auto stop() noexcept -> void { is_active_ = false, reset(); }
+  constexpr auto tick( uint64_t ms_since_last_tick ) noexcept -> RetransmissionTimer&
+  {
+    timer_ += is_active_ ? ms_since_last_tick : 0;
+    return *this;
+  }
+
+private:
+  bool is_active_ {};
+  uint64_t RTO_ms_;
+  uint64_t timer_ {};
+};
+
 
 class TCPSender
 {
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
-  {}
-
+    : timer_(initial_RTO_ms),input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
+  {};
+  
   /* Generate an empty TCPSenderMessage */
   TCPSenderMessage make_empty_message() const;
 
@@ -26,7 +53,8 @@ public:
   /* Push bytes from the outbound stream */
   void push( const TransmitFunction& transmit );
 
-  /* Time has passed by the given # of milliseconds since the last time the tick() method was called */
+  /* Time has passed by the given # of mill
+  iseconds since the last time the tick() method was called */
   void tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit );
 
   // Accessors
@@ -35,11 +63,21 @@ public:
   const Writer& writer() const { return input_.writer(); }
   const Reader& reader() const { return input_.reader(); }
   Writer& writer() { return input_.writer(); }
-
-private:
   Reader& reader() { return input_.reader(); }
 
-  ByteStream input_;
-  Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+private:
+RetransmissionTimer timer_;
+ByteStream input_;
+Wrap32 isn_;
+uint64_t initial_RTO_ms_{};
+std::queue<TCPSenderMessage> outstanding_message_{};
+uint64_t total_retransmission_{};
+uint64_t rck_no{};
+uint64_t next_abs_seqno_{};
+  uint64_t ack_abs_seqno_{};
+  uint64_t _windos_size = {1};
+  uint64_t pushed_count = {};
+  uint64_t _bytes_in_flight = {};
+  bool FIN_sent_={};
+  bool SYN_sent_={};
 };
